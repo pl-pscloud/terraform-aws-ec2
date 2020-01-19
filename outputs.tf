ba087d922cde8b5b7785344042b213b547d394d2
pswitala <p.switala@pslab.eu>
2020-01-19 09:10:41 +0100

    Chnaged output var for public IP (export dynamic public or eip)

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,5 @@
-output "pscloud_eip" {
-  value = aws_eip.pscloud-eip[*].public_ip
-}
-
 output "pscloud_ip" {
-  value = aws_instance.pscloud-ec2[*].public_ip
+  value = (var.pscloud_eip_true ? aws_eip.pscloud-eip[*].public_ip : aws_instance.pscloud-ec2[*].public_ip)
 }
 
 output "pscloud_ids" {
